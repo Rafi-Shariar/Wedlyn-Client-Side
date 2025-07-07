dd344ce85045389eae5b3dd69f9700c7e05ec6bf
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-07-07 21:56:01 +0600

    project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { RouterProvider } from "react-router";
-import router from "./Routes/Route.jsx";
-// import AuthProvider from "./Context/AuthProvider.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 
-createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <AuthProvider> */}
-      <RouterProvider router={router}>
-      </RouterProvider>
-    {/* </AuthProvider> */}
-  </StrictMode>
-);
+    <RouterProvider router={router}><App/></RouterProvider>
+  </StrictMode>,
+)
