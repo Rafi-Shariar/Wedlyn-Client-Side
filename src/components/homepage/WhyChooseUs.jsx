ac2fbdcad835ae05ec4d2e2ccb9dab56e41fd8a5
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-08-16 15:54:15 +0600

    choose us cards updated

--- src/components/homepage/WhyChooseUs.jsx
+++ src/components/homepage/WhyChooseUs.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import secureIMG from '../../assets/whyChooseUs/encrypted.png';
+import moneyIMG from '../../assets/whyChooseUs/affordable.png';
+import personalIMG from '../../assets/whyChooseUs/heart.png';
 const WhyChooseUs = () => {
   return (
     <div>
@@ -14,7 +17,30 @@ const WhyChooseUs = () => {
         </p>
       </div>
 
-      
+      <section className="grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-6 max-w-4xl mx-auto">
+
+        <div className="flex flex-col justify-center items-center border p-6 rounded-lg border-purple-300 shadow-lg">
+            <img src={secureIMG} alt="" className="w-14"/>
+            <h1 className="text-lg font-semibold">Trusted & Secure Platform</h1>
+            <p className="text-xs text-gray-500">Every profile is verified and protected</p>
+        </div>
+
+        <div className="flex flex-col justify-center items-center border p-6 rounded-lg border-purple-300 shadow-lg">
+            <img src={moneyIMG} alt="" className="w-14"/>
+            <h1 className="text-lg font-semibold">Affordable & Transparent</h1>
+            <p className="text-xs text-gray-500">No hidden costs for lifetime</p>
+        </div>
+
+        <div className="flex flex-col justify-center items-center border p-6 rounded-lg border-purple-300 shadow-lg">
+            <img src={personalIMG} alt="" className="w-14"/>
+            <h1 className="text-lg font-semibold">ersonalized Matchmaking</h1>
+            <p className="text-xs text-gray-500">Save favorites, track requests</p>
+        </div>
+
+
+      </section>
+
+
     </div>
   );
 };
